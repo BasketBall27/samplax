33a5047a6575e8e4493562d23ad2e978f11800ea
Laterium <97043303+laterium@users.noreply.github.com>
2025-01-11 21:53:55 -0800

    Update install-java.cmd

--- install-java.cmd
+++ install-java.cmd
@@ -1,1 +1,6 @@
+:: Contributors 2024-2025 Anonim "Socket"
+@echo off
+for /f "delims=" %%i in ('winget -v') do set version=%%i
+title winget %version%
 winget install -e --id Oracle.JavaRuntimeEnvironment
+pause
